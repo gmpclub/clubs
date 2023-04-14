c4be22e3d1c0db8831cb6737e4dad52e9383ac53
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2023-04-14 14:38:10 +0200

    Home Page and Satzung

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,195 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="bg-white text-black">
+      <div className="w-full h-full flex flex-1">
+        <div className="min-h-screen flex-1 bg-image-1">
+          <div className="flex flex-1 h-full bg-black bg-opacity-50 justify-center items-center">
+            <Image src="/grashaus_logo.png" alt="GrasHaus Logo" width={500} height={500} />
+          </div>
+        </div>
+        <div className="min-h-screen flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full justify-center items-center flex-col p-8">
+            <Image src="/grashaus_green_logo.png" alt="GrasHaus Logo" width={300} height={300} />
+            <h1 className="text-3xl font-extrabold text-center -mt-4">
+              Willkommen zur Registrierung im ersten Cannabis Club deiner Stadt.
+            </h1>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full h-full flex flex-1">
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col my-16">
+            <div>
+              <h3 className="text-3xl font-bold mb-6">Unsere Ziele:</h3>
+              <ul className="list-disc mb-4 text-xl">
+                <h6 className="font-bold">Mitgestaltung der Legalisierung:</h6>
+                <li className="ml-6">Demokratisch, einfach, mit wenig eigenem Aufwand </li>
+              </ul>
+              <ul className="list-disc text-xl">
+                <h6 className="font-bold">Nach Erfolg der Legalisierung:</h6>
+                <li className="ml-6">
+                  <span className="font-bold">Anbau</span> im Verein, aktiv oder nicht-aktiv
+                </li>
+                <li className="ml-6">Selbstversorgung mit Cannabis und Erzeugnisse daraus</li>
+                <li className="ml-6">Anbauwissen, Qualitätssicherung und Jugendschutz</li>
+              </ul>
+              <div className="flex flex-1 mt-8 justify-center">
+                <a href="#registration" className="btn btn-active btn-primary">
+                  Kostenlos Mitglied werden!
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-image-2">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full h-full flex flex-1">
+        <div className="flex-1 bg-image-6">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col m-16">
+            <div>
+              <h3 className="text-3xl font-bold mb-6">GMP Cannabis Club</h3>
+              <p>
+                Wir sind ein eingetragener Verein in Stuttgart mit dem Ziel, unsere Mitglieder nach
+                der Legalisierung durch gemeinschaftlichen Selbstanbau mit Cannabis zu versorgen.
+                Politik, Bildung und Verwaltung finden im GMP Cannabis Club kompetente
+                Ansprechpartner für die Herausforderungen der fortschreitenden Legalisierung von
+                Cannabis.
+              </p>
+              <br />
+              <p>
+                Unser Handeln orientiert sich an der ENCOD-Kampagne für freie Cannabis Clubs. Dabei
+                profitieren wir von den Erfahrungen, unserer Erfahrung in Amerika, Uruguay, Spanien,
+                Belgien und den Niederlanden und Thailand die wir seit 2009 gesammelt haben - und
+                wir hier beste Kontakte pflegen. Wir streben eine demokratische Zusammenarbeit aller
+                CSCs in Deutschland an.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-1">
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col m-16">
+            <div>
+              <h3 className="text-3xl font-bold mb-6 mt-8">Was machen wir?</h3>
+              <p>
+                Was machen wir? Wir erforschen, produzieren, und konsumieren bereits seit mehreren
+                Jahren Cannabis und Medizinal Cannabis. Diese Erfahrung geben wir an Clubmitgleider
+                weiter, um Sie zu befähigen, bei Bedarf Ihren eigenen Club mit Freunden zu gründen.
+                Satzung des Clubs:
+              </p>
+              <div className="flex flex-1 mt-8 justify-center">
+                <Link href="/satzung" className="btn btn-active btn-primary">
+                  Satzung
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-image-5">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-1">
+        <div className="flex-1 bg-image-4">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col m-16">
+            <div>
+              <h3 className="text-3xl font-bold mb-6">Mitgliederschutz</h3>
+              <p>
+                Mitgliederdaten werden nur anonymisiert nach außen gegeben. Die Mitglieder können
+                nach Erfolg der Legalisierung von Cannabis in geschützten, sicheren Räumen
+                Hanfprodukte erwerben. Beratung und Begleitung sind, wenn gewünscht, möglich.
+                Eventuell problematisches Konsumverhalten von Mitgliedern wird frühzeitig bemerkt.
+                Die Qualität der Produkte ist sauber und hoch. Bekömmlichere Konsumformen als
+                Rauchen werden gefördert und gelehrt. Soziales Miteinander wird geschützt und
+                gefördert.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-1" id="registration">
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col m-16">
+            <form className="w-full">
+              <h3 className="text-3xl font-bold mb-6 mt-8">Hier registrieren!</h3>
+              <div className="flex flex-1 gap-4">
+                <input
+                  type="text"
+                  name="firstname"
+                  placeholder="Vorname"
+                  className="input input-bordered input-primary w-full my-2 bg-white"
+                />
+                <input
+                  type="text"
+                  name="lastname"
+                  placeholder="Name"
+                  className="input input-bordered input-primary w-full my-2 bg-white"
+                />
+              </div>
+              <div className="flex flex-1 gap-4">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered input-primary w-full my-2 bg-white"
+                />
+                <input
+                  type="text"
+                  name="phoneNumber"
+                  placeholder="Mobil Nummer"
+                  className="input input-bordered input-primary w-full my-2 bg-white"
+                />
+              </div>
+              <input
+                type="text"
+                name="city"
+                placeholder="Stadt"
+                className="input input-bordered input-primary w-full my-2 bg-white"
+              />
+              <div className="flex flex-1 gap-4 mx-2">
+                <div className="my-2 flex items-center gap-2">
+                  <input type="radio" name="membershipType" className="radio radio-primary" />
+                  <label htmlFor="membershipType">Aktives Mitglied</label>
+                </div>
+                <div className="my-2 flex items-center gap-2">
+                  <input type="radio" name="membershipType" className="radio radio-primary" />
+                  <label htmlFor="membershipType">Nicht aktives Mitglied</label>
+                </div>
+                <div className="my-2 flex items-center gap-2">
+                  <input type="radio" name="membershipType" className="radio radio-primary" />
+                  <label htmlFor="membershipType">Ich bin mir noch nicht sicher</label>
+                </div>
+              </div>
+              <p className="text-sm text-gray-700">
+                Aktives Mitglied: Beteilige dich beim Anbau mit max. 10 Stunden je Woche.
+              </p>
+              <p className="text-sm text-gray-700 mb-4">
+                Nicht aktives Mitglied: Erwerbe Genussmittel ohne zeitlichen Aufwand, weil andere
+                Clubmitglieder deine Pflanzen aufziehen und verarbeiten
+              </p>
+              <button className="btn btn-block btn-primary">Abschicken</button>
+            </form>
+          </div>
+        </div>
+        <div className="flex-1 bg-image-3">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
---
+++ pages/satzung.tsx
@@ -0,0 +1,68 @@
+import Image from "next/image";
+
+const Satzung = () => {
+  return (
+    <main className="bg-white text-black">
+      <div className="w-full h-full flex flex-1">
+        <div className="min-h-screen flex-1 bg-image-satzung-1">
+          <div className="flex flex-1 h-full bg-black bg-opacity-50 justify-center items-center">
+            <Image src="/grashaus_logo.png" alt="GrasHaus Logo" width={500} height={500} />
+          </div>
+        </div>
+        <div className="min-h-screen flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full justify-center items-center flex-col p-8">
+            <Image src="/grashaus_green_logo.png" alt="GrasHaus Logo" width={300} height={300} />
+            <h1 className="text-3xl font-extrabold text-center -mt-4">
+              Satzung des GMP Cannabis Social Club
+            </h1>
+            <a
+              href="/20230401_Vereinssatzung_GMPC_V1"
+              className="btn btn-active btn-primary mt-8"
+              download
+            >
+              Download
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="w-full h-full flex flex-1">
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 h-full items-center flex-col my-16">
+            <div className="m-16">
+              <h3 className="text-3xl font-bold mb-4">Präambel:</h3>
+              <p>
+                Cannabis Social Clubs (CSC) sind Anbaugemeinschaften von Cannabisnutzern, die ihren
+                Eigenbedarfsanbau gemeinschaftlich organisieren oder, dort wo Anbau von Cannabis
+                noch nicht erlaubt ist, die Legalisierung des Konsums und des Anbaus von Cannabis
+                zum Eigenbedarf anstreben.
+              </p>
+              <br />
+              <p>
+                Da der Anbau von THC-haltigem Hanf, auch für den Eigenbedarf in der Bundesrepublik
+                Deutschland immer noch verboten ist, und auch aktiv verfolgt wird, werden die
+                Aufgaben des GMP Cannabis Social Club Vereins zunächst darin bestehen, sich als
+                Interessengemeinschaften von Cannabis-Konsumenten einzusetzen für:
+              </p>
+              <br />
+              <ul className="list-disc pl-6">
+                <li>Die Änderung der Drogengesetzgebung in Deutschland</li>
+                <li>Eine akzeptierende und regulierende Drogenpolitik in Stuttgart</li>
+                <li>Jugend- und Verbraucherschutz</li>
+                <li>Aufklärung und Prävention</li>
+                <li>
+                  Nach der Legalisierung und Schaffung der gesetzeskonformen Möglichkeit, strebt der
+                  Cannabis Social Club den Betrieb einer, dann legalen, Anbaugemeinschaft der
+                  Mitglieder an.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 bg-image-satzung-2">
+          <div className="flex flex-1 h-full bg-black bg-opacity-20 justify-center items-center"></div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Satzung;
