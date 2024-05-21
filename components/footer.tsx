d5bcde4bd1e51aeb7131d81e3a927b416a738bf9
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2024-05-21 20:54:44 +0200

    instagram link

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,27 +4,27 @@ import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-1 flex-col-reverse lg:flex-row justify-between items-center w-full bg-white border-t-primary border-t-2">
-      <div className="mx-16 flex items-center">
+    <footer className="flex flex-col-reverse items-center justify-between flex-1 w-full bg-white border-t-2 lg:flex-row border-t-primary">
+      <div className="flex items-center mx-16">
         <Link href="/">
           <Image src="/grashaus_green_logo.png" alt="GrasHaus Logo" width={200} height={200} />
         </Link>
-        <span className="text-primary-focus text-sm">
+        <span className="text-sm text-primary-focus">
           &copy; GRASHAUS Club e.V. {new Date().getFullYear()}
         </span>
       </div>
-      <div className="mx-16 flex items-center gap-4 mt-8 lg:mt-0 ">
-        <Link href="/imprint" className="text-primary text-sm">
+      <div className="flex items-center gap-4 mx-16 mt-8 lg:mt-0 ">
+        <Link href="/imprint" className="text-sm text-primary">
           Impressum
         </Link>
-        <Link href="/data-protection" className="text-primary text-sm">
+        <Link href="/data-protection" className="text-sm text-primary">
           Datenschutzhinweise DSGVO
         </Link>
         <a
-          href="https://instagram.com/highend_smoking_thailand?igshid=YmMyMTA2M2Y="
+          href="https://www.instagram.com/grashausworldwide.official?igsh=MTNmMjJ5cnNyMHdmbA=="
           target="_blank"
         >
-          <div className="bg-primary-focus rounded-full p-2">
+          <div className="p-2 rounded-full bg-primary-focus">
             <AiOutlineInstagram className="text-white" size={30} />
           </div>
         </a>
